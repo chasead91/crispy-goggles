af13c99ecceeeb8ca391096ee682c7743d14c40d
Chase Aderhold <chase.aderhold@gmail.com>
2026-07-23 20:05:15 -0400

    update lobby view source code

--- Lobby View.sql
+++ Lobby View.sql
@@ -3,9 +3,11 @@ select
     si.name AS 'Sitter',
     r.name AS 'Reader',
     r.offering AS 'Session Type',
-    s.queue_position AS 'Line Position',
+    s.created_at AS 'Created At',
     s.status AS 'Session Status'
 from session s
 left join reader r on s.reader_id = r.reader_id
 left JOIN sitter si on s.sitter_id = si.sitter_id
 where s.status != 'Done';
+
+delete from session;
